cf18ed6545a9ef094ba305c8df74ea508d164d2e
jay <jay@contensive.com>
2019-09-19 14:29:16 -0400

    create build scripts. renamed solution and folder to the aoSolution standard

---
+++ scripts/Env.cmd
@@ -0,0 +1,11 @@
+
+rem all paths are relative to the git scripts folder
+
+set appName=designblocks
+set majorVersion=5
+set minorVersion=1
+set collectionName=aoToolPanel
+set collectionPath=..\collections\aoToolPanel\
+set binPath=..\source\aoToolPanel\bin\debug\
+set msbuildLocation=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\
+set deploymentFolderRoot=C:\Users\jay\Desktop\deployments\aoToolPanel\Dev\
